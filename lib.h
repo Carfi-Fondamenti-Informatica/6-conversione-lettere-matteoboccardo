c8d5dba628de0c246ff252bcb1a69e55279f3ff4
matteoboccardo <91879241+matteoboccardo@users.noreply.github.com>
2021-12-01 18:31:38 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,10 @@
+//
+// Created by bocca on 1/12/21.
+//
 
+#ifndef ESERCITAZIONE_6_LIBRERIA_H
+#define ESERCITAZIONE_6_LIBRERIA_H
+
+#endif //ESERCITAZIONE_6_LIBRERIA_H
+
+bool conversione (char&);
